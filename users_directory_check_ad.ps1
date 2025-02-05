10711b3020fc172737652801fbc06d08451ef687
Loyds62 <renaudroussel@icloud.com>
2025-02-05 15:29:08 +0100

    Add files via upload

--- users_directory_check_ad.ps1
+++ users_directory_check_ad.ps1
@@ -8,15 +8,7 @@ function Red
     process { Write-Host $_ -ForegroundColor Red }
 }
 
-function Remove-Folder {
-    param (
-        [string]$folderPath
-    )
-    Remove-Item -Path $folderPath -Recurse -Force
-    Write-Output "Dossier $folderPath supprimé." | Red
-}
-
-while ($true) {
+do {
     # Demander à l'utilisateur de saisir le nom du poste
     $pc = Read-Host "Veuillez saisir le nom du poste"
     $path="\\$pc\c$\users"
@@ -32,20 +24,23 @@ while ($true) {
 
     # Vérifier chaque dossier utilisateur dans Active Directory
     foreach ($user in $userFolders) {
-        if ($user -ne "temp" -and $user -ne "public") {
+        if ($user -ne "temp" -and $user -ne "public" -and $user -ne "Administrateur" -and $user -ne "administrator" -and $user -ne "default" -and $user -ne "postgres") {
             $adUser = Get-ADUser -Filter {SamAccountName -eq $user} -ErrorAction SilentlyContinue
             if ($adUser) {
                 Write-Output "Utilisateur $user (login: $($adUser.SamAccountName)) existe dans Active Directory." | Green
             } else {
                 Write-Output "Utilisateur $user n'existe pas dans Active Directory." | Red
-                Remove-Folder -folderPath "$path\$user"
+                $confirm = Read-Host "Voulez-vous supprimer le dossier de l'utilisateur $user ? (o/n)"
+                if ($confirm -eq 'o') {
+                    Remove-Item -Path "$path\$user" -Recurse -Force
+                    Write-Output "Dossier de l'utilisateur $user supprimé." | Green
+                } else {
+                    Write-Output "Dossier de l'utilisateur $user conservé." | Red
+                }
             }
         }
     }
 
-    # Demander à l'utilisateur s'il veut recommencer
-    $recommencer = Read-Host "Voulez-vous recommencer ? (oui/non)"
-    if ($recommencer -ne "oui") {
-        break
-    }
-}
+    # Demander à l'utilisateur s'il veut traiter un autre poste
+    $continue = Read-Host "Voulez-vous traiter un autre poste ? (o/n)"
+} while ($continue -eq 'o')
