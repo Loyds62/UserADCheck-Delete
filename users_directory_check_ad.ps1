aa019111bd90b1069d280bc5ac60d06b98ac6120
Loyds62 <renaudroussel@icloud.com>
2025-02-05 16:01:28 +0100

    Update users_directory_check_ad.ps1

--- users_directory_check_ad.ps1
+++ users_directory_check_ad.ps1
@@ -1,46 +1,43 @@
-﻿function Green
-{
+ function Green {
     process { Write-Host $_ -ForegroundColor Green }
 }
 
-function Red
-{
+function Red {
     process { Write-Host $_ -ForegroundColor Red }
 }
 
 do {
-    # Demander à l'utilisateur de saisir le nom du poste
     $pc = Read-Host "Veuillez saisir le nom du poste"
-    $path="\\$pc\c$\users"
+    $path = "\\$pc\c$\users"
 
-    # Vérifier si le chemin existe
     if (-Not (Test-Path -Path $path)) {
-        Write-Output "Le chemin spécifié n'existe pas. Veuillez vérifier et réessayer."
+        Write-Output "Le chemin spécifié n'existe pas. Veuillez vérifier et réessayer." | Red
         continue
     }
 
-    # Obtenir la liste des dossiers utilisateurs
     $userFolders = Get-ChildItem -Path $path -Directory | Select-Object -ExpandProperty Name
 
-    # Vérifier chaque dossier utilisateur dans Active Directory
     foreach ($user in $userFolders) {
-        if ($user -ne "temp" -and $user -ne "public" -and $user -ne "Administrateur" -and $user -ne "administrator" -and $user -ne "default" -and $user -ne "postgres") {
+        if ($user -notin @("temp", "public", "Administrateur", "administrator", "default", "postgres")) {
             $adUser = Get-ADUser -Filter {SamAccountName -eq $user} -ErrorAction SilentlyContinue
             if ($adUser) {
                 Write-Output "Utilisateur $user (login: $($adUser.SamAccountName)) existe dans Active Directory." | Green
             } else {
                 Write-Output "Utilisateur $user n'existe pas dans Active Directory." | Red
-                $confirm = Read-Host "Voulez-vous supprimer le dossier de l'utilisateur $user ? (o/n)"
+                $confirm = Read-Host "Voulez-vous supprimer le contenu du dossier de l'utilisateur $user ? (o/n)"
                 if ($confirm -eq 'o') {
-                    Remove-Item -Path "$path\$user" -Recurse -Force
-                    Write-Output "Dossier de l'utilisateur $user supprimé." | Green
+                    try {
+                        Get-ChildItem -Path "$path\$user" -Recurse | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+                        Write-Output "Contenu du dossier de l'utilisateur $user supprimé." | Green
+                    } catch {
+                        Write-Output "Erreur lors de la suppression du contenu du dossier de l'utilisateur $user : $_" | Red
+                    }
                 } else {
-                    Write-Output "Dossier de l'utilisateur $user conservé." | Red
+                    Write-Output "Contenu du dossier de l'utilisateur $user conservé." | Red
                 }
             }
         }
     }
 
-    # Demander à l'utilisateur s'il veut traiter un autre poste
     $continue = Read-Host "Voulez-vous traiter un autre poste ? (o/n)"
 } while ($continue -eq 'o')
